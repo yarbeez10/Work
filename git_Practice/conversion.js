796d6e9a11ecd34ce15502f04fbb3072edc43149
yarbeez <yarbeez10@gmail.com>
2026-09-17 19:48:04 +0500

    Practice of conversion of Values into different types

---
+++ git_Practice/conversion.js
@@ -0,0 +1,26 @@
+let num = 142
+let string = "132"
+let boolean = true
+let nullValue = null
+
+console.log(num)
+
+let convertedNumber = String(num)
+console.log(convertedNumber);
+console.log(typeof(convertedNumber));
+
+let convertedString = Number(string)
+console.log(convertedString);
+console.log(typeof(convertedString));
+
+let convertedBoolean = Number(boolean)
+console.log(convertedBoolean);
+console.log(typeof(convertedBoolean));
+
+let convertedNull = Number(nullValue)
+console.log(convertedNull);
+console.log(typeof(convertedNull));
+
+let convertedNull2 = String(nullValue)
+console.log(convertedNull2);
+console.log(typeof(convertedNull2));
