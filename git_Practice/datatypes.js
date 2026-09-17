7519b0a2b310df817ba89de06ebde2d03bb62340
yarbeez <yarbeez10@gmail.com>
2026-09-17 19:09:20 +0500

    Practice of understanding data types and how to find out data types in JavaScript.

---
+++ git_Practice/datatypes.js
@@ -0,0 +1,20 @@
+let num = 24
+let name = "Haris"
+
+console.log(num)
+console.log(name)
+
+/*
+    different types of databases
+    number is upto 2 to the power of 53
+    bigint is for larger numbers
+    string is used by ""
+    null is used when there is no value and works standalone
+    symbol is used for unique identifiers
+
+    in order to identify type of variable we use typeof operator
+*/
+
+console.log(typeof(num));
+console.log(typeof(name));
+
