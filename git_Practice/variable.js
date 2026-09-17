af68ac5169e411c2bb5f68fea068b0d593f4d114
yarbeez <yarbeez10@gmail.com>
2026-09-17 18:24:28 +0500

    Practice for uploading files to git repository

---
+++ git_Practice/variable.js
@@ -0,0 +1,16 @@
+const accountID = 141231
+let accountName = "Hassan"
+var accountRank = 42    
+
+accountCity = "Islamabad" 
+
+console.log(accountName);
+console.log(accountRank);
+
+accountID = 423132
+
+console.log(accountID);
+
+/*
+    Do not use var since most of the things can be done with let, const value cannot be changed. 
+*/
